775498f96d607229a77bf55771ca47703d6ea921
cristiands1109 <cristiands1109@gmail.com>
2019-09-29 17:20:50 -0400

    Seccion 13: Carga de Archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -57,8 +57,37 @@ let verificaADMIN_ROLE = (req, res, next) => {
 };
 
 
+//===================================================================//
+//====================VERIFICA TOKEN PARA IMG=========================//
+//===================================================================//
+
+let verificaTOKEN_IMG = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                error: {
+                    menesaje: 'Token invalido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+
+}
+
+
 
 module.exports = {
     verificaToken,
-    verificaADMIN_ROLE
+    verificaADMIN_ROLE,
+    verificaTOKEN_IMG
 }
---
+++ server/routes/imagenes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+
+const path = require('path');
+
+const {} = require('../middlewares/autenticacion')
+
+const app = express();
+
+
+
+app.get('/imagen/:tipo/:img', (req, res) => {
+
+    let tipo = req.params.tipo;
+    let img = req.params.img
+
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
+
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+
+        let noImagePath = path.resolve(__dirname, '../assets/no-image-found.jpg');
+        res.sendFile(noImagePath);
+
+    }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
